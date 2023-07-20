41aa96536a6429276e583a441da8cfb0630eb08e
merrycoral <merrycoral@gmail.com>
2023-07-20 13:55:11 +0900

    날아간 코드 복구

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,52 +3,98 @@
     <Logo />
     <div class="nav nav-pills">
       <div v-for="nav in navigations" :key="nav.name" class="nav-item">
-        <RouterLink :to="nav.href" 
-        active-class="active"
-        class="nav-link">
+        <RouterLink
+          :to="nav.href"
+          active-class="active"
+          :class="{ active: isMatch(nav.path) }"
+          class="nav-link"
+        >
           {{ nav.name }}
         </RouterLink>
       </div>
+    </div>
+    <div class="user" @click="toAbout">
+      <img :src="$store.state.about.image" alt="HEROPY" />
     </div>
   </header>
 </template>
 
 <script>
-import Logo from '~/components/Logo'
+import Logo from "~/components/Logo";
 
 export default {
   components: {
-    Logo
+    Logo,
   },
   data() {
     return {
       navigations: [
         {
-          name: 'Search',
-          href: '/'
+          name: "Search",
+          href: "/",
         },
         {
-          name: 'Movie',
-          href: '/movie'
+          name: "Movie",
+          href: "/movie/tt4520988",
+          path: /^\/movie/,
         },
         {
-          name: 'About',
-          href: '/about'
-        }
-      ]
-    }
-  }
-}
+          name: "About",
+          href: "/about",
+        },
+      ],
+    };
+  },
+  methods: {
+    isMatch(path) {
+      // if (!path) { return false }
+      // Optional chaining
+      return path?.test(this.$route.fullPath);
+    },
+    toAbout() {
+      this.$router.push("/about");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
+
 header {
   height: 70px;
-  padding: 0 40px;
   display: flex;
   align-items: center;
+  padding: 0 40px;
+  position: relative;
   .logo {
     margin-right: 40px;
+  }
+  .user {
+    width: 40px;
+    height: 40px;
+    padding: 6px;
+    box-sizing: border-box;
+    border-radius: 50%;
+    background-color: $gray-200;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 40px;
+    margin: auto;
+    transition: 0.4s;
+    &:hover {
+      background-color: darken($gray-200, 10%);
+    }
+    img {
+      width: 100%;
+    }
+  }
+  @include media-breakpoint-down(sm) {
+    .nav {
+      display: none;
+    }
   }
 }
 </style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,27 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import Home from './Home'
-import About from './About'
-import Movie from './Movie'
+import Home from "./Home";
+import About from "./About";
+import Movie from "./Movie";
 
 export default createRouter({
-    // Hash
-    // https://google.com/#/search
-    history: createWebHashHistory(),
-    // pages
-    // https://google.com/
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/movie',
-            component: Movie
-        },
-        {
-            path: '/about',
-            component: About
-        }
-    ]
-})
+  // Hash
+  // https://google.com/#/search
+  history: createWebHashHistory(),
+  // pages
+  // https://google.com/
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/movie/:id?",
+      component: Movie,
+    },
+    {
+      path: "/about",
+      component: About,
+    },
+  ],
+});
